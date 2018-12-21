a151ebbf6ac24c52bb1694ba871aeced3a4e6200
sakura.zen7 <sakurazen7@gmail.com>
2018-12-21 21:39:08 +0000

    kadai-lesson12

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+
   def index
     @tasks = Task.all.page(params[:page])
   end
@@ -12,14 +14,14 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
     else
-      flash[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
   
@@ -40,7 +42,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] ='Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private
